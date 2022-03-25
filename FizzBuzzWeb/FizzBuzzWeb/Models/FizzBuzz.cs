92677b5c95779a8629c5fa41f643b8793ced14d7
KrzysztofKapliński <treacheuw@gmail.com>
2022-03-25 02:54:42 +0100

    Zmiany do zadania 2, dodanie formularzy

---
+++ FizzBuzzWeb/FizzBuzzWeb/Models/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage ="Pole jest obowiązkowe")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość pomiędzy 1 a 1000")]
+        public int? Number { get; set; }
+    }
+
+}
--- FizzBuzzWeb/FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb.Models;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public string Alert { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +23,48 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
+            Alert = String.Empty;
 
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Alert = String.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                CheckNumber();
+                return Page();
+            }
+
+            return Page();
+        }
+
+        public void CheckNumber()
+        {
+            if (FizzBuzz.Number % 3 == 0)
+            {
+                if (FizzBuzz.Number % 5 == 0)
+                {
+                    Alert = "FizzBuzz";
+                }
+                else
+                {
+                    Alert = "Fizz";
+                }
+            }
+            else if (FizzBuzz.Number % 5 == 0)
+            {
+                Alert = "Buzz";
+            }
+            else
+            {           
+                Alert = "Liczba: " + FizzBuzz.Number.ToString() + " nie spełnia kryteriów FizzBuzz";
+            }
         }
     }
 }
